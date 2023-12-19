37d1824729776ff37ff472e5634eeae2424652e7
Freakman14 <freemem13@gmail.com>
2023-12-19 15:38:17 +0500

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿string a = Console.ReadLine();
+string b = Console.ReadLine();
+int c = Convert.ToInt32(Console.ReadLine());
+
+var person = new Person();
+person.FirstName = a;
+person.LastName = b;
+person.Age = c;
+
+
+System.Console.WriteLine(person.GetInfo());
+System.Console.WriteLine(person.GetInfo2());
+System.Console.WriteLine(person.GetInfo3());
+
+
+class Person
+{
+    public string FirstName;
+    public string GetInfo()
+    {
+        return $"FirstName: {FirstName}";
+    }
+    public string LastName;
+    public string GetInfo2()
+    {
+        return $"LastName: {LastName}";
+    }
+    public int Age;
+    public string GetInfo3()
+    {
+        return $"Age: {Age}";
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+var post = new Post();
+post.Text = "It is Post";
+post.Likes = 1;
+post.Comments = "Super";
+post.Average = "Great";
+
+System.Console.WriteLine(post.SetPost());
+System.Console.WriteLine(post.AddLike());
+System.Console.WriteLine(post.AddComment());
+System.Console.WriteLine(post.GetInfo());
+
+
+
+class Post
+{
+    public string Text;
+    public string SetPost()
+    {
+        return $"Text = {Text}";
+    }
+    public int Likes;
+    public string AddLike()
+    {
+        return $"Likes : {Likes + 1}";
+    }
+    public string Comments;
+    public string AddComment()
+    {
+        return $"Comments : {Comments}";
+    }
+    public string Average;
+    public string GetInfo()
+    {
+        return $"{Average}";
+    }
+
+
+}
